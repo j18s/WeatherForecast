6ea79b6437e9a6cec8b9c2b0c2825b08862f6791
sumit jolly <sumit_jolly@mckinsey.com>
2017-05-06 07:47:38 +0530

    added tests

--- routes/index.js
+++ routes/index.js
@@ -3,44 +3,36 @@ var router = express.Router();
 var request = require('request');
 var helper = require('../helper/helper.js');
 var config = require('../config/config.js');
-var weather = {};
 
 var configData = config.getConfig();
-var base_url = helper.findKeyValue(configData,'baseurl');
+process.env.PORT = helper.findKeyValue(configData, 'port');
+var base_url = helper.findKeyValue(configData, 'baseurl') + ':' + process.env.PORT;
 
+console.log(base_url);
 /* GET home page. */
+
 router.get('/', function (req, res, next) {
-  res.render('test ok', {});
+  request.get(base_url + '/weatherForecast', function (err, response, body) {
+    let weather = body;
+    weather = JSON.parse(body);
+    let output = helper.getDates(weather);
+    let result = { plotdata: output, title: weather.city.name, datetime: new Date(), unit: "Celsius" };
+    res.status = 200;
+    res.render('index', result);
+  })
 });
 
-router.get('/weather', function (req, res, next) {
-  // request.get(base_url+'/weatherForecast', function (err, response, body) {
-  //   weather = body;
-  //   weather =JSON.parse(body);
-
-  //   //**Uncomment below line with actual */
-  //  // weather=JSON.parse(weather);
-
-  //   let output = helper.getDates(weather);
-  //   let result = {plotdata: output, title: weather.city.name, datetime: new Date(), unit: "Celsius"};
-  //   res.render('index', result);
-  // })
-  
-  res.render('test', {});
-});
 router.get('/weatherForecast', function (req, res, next) {
-  //weatherObj = () => {
-  //TODO: replace URL
-  //let jsonContent = C.getFileDetails('./config/config.json');
-  //let url = jsonContent.weatherApi + jsonContent.city + '&appid=' + jsonContent.appid + '&units=' + jsonContent.unit;
-  // console.log(url);
-  // request.get(url, function (error, response, body) {
-  //   res.json(body);
-  // });
+  //console.log(configData);
+  let url = helper.findKeyValue(configData, 'weatherApi') +
+    helper.findKeyValue(configData, 'city') + '&appid=' +
+    helper.findKeyValue(configData, 'appid') + '&units=' +
+    helper.findKeyValue(configData, 'unit');
 
-  let data = helper.getFileDetails('./json/forecast.json');
-  //console.log(data);
-
-  res.json(data);
+  request.get(url, function (error, response, body) {
+    res.setHeader('Content-Type', 'application/json');
+    body = JSON.parse(body);
+    res.json(body);
+  });
 });
 module.exports = router;
